380d9737b551a58420252aac71360dabafd6625c
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-09-26 14:03:42 +0530

    Get single product working

--- src/routes/ProductRoutes.js
+++ src/routes/ProductRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
 
-import { createProduct, getAllProducts } from "../controllers/ProductControllers.js";
+import { createProduct, getAllProducts, getSingleProduct } from "../controllers/ProductControllers.js";
 
 router.post("/create-product", createProduct);
 router.get("/all-products", getAllProducts);
+router.get("/:productId", getSingleProduct);
 
 export default router;
